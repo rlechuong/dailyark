e9217682a5c543a646a01aa8646cc465b9ff9e29
Richard Le <169632088+rlechuong@users.noreply.github.com>
2025-07-23 14:31:30 -0400

    Remove Daily Una Tasks

--- includes/json/DC.js
+++ includes/json/DC.js
@@ -1,44 +1,23 @@
 var dailychar = {
-    "guild-activity": {
-        task: "Guild Activity",
-        url: "#",
-        short: true,
-        desc: "Placeholder",
-        img: "guild-activity"
-    },
-    "daily-una-task-1": {
-        task: "Daily Una Task 1",
-        url: "#",
-        short: true,
-        desc: "Placeholder",
-        img: "daily-una"
-    },
-    "daily-una-task-2": {
-        task: "Daily Una Task 2",
-        url: "#",
-        short: true,
-        desc: "Placeholder",
-        img: "daily-una"
-    },
-    "daily-una-task-3": {
-        task: "Daily Una Task 3",
-        url: "#",
-        short: true,
-        desc: "Placeholder",
-        img: "daily-una"
-    },
-    "kurzan-front": {
-        task: "Kurzan Front",
-        url: "#",
-        short: true,
-        desc: "Placeholder",
-        img: "kurzan-front"
-    },
-    "guardian-raid": {
-        task: "Guardian Raid",
-        url: "#",
-        short: true,
-        desc: "Placeholder",
-        img: "guardian-raid"
-    },
+  "guild-activity": {
+    task: "Guild Activity",
+    url: "#",
+    short: true,
+    desc: "Placeholder",
+    img: "guild-activity",
+  },
+  "kurzan-front": {
+    task: "Kurzan Front",
+    url: "#",
+    short: true,
+    desc: "Placeholder",
+    img: "kurzan-front",
+  },
+  "guardian-raid": {
+    task: "Guardian Raid",
+    url: "#",
+    short: true,
+    desc: "Placeholder",
+    img: "guardian-raid",
+  },
 };
